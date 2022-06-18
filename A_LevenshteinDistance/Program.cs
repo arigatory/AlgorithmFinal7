0b7338c741d08e061ce799bf92f25c1465f83408
Ivan MSI <arigatory@gmail.com>
2022-06-18 14:47:40 +0300

    fix

--- A_LevenshteinDistance/Program.cs
+++ A_LevenshteinDistance/Program.cs
@@ -1,4 +1,4 @@
-﻿// https://contest.yandex.ru/contest/25597/run-report/68967337/
+﻿// https://contest.yandex.ru/contest/25597/run-report/69025087/
 
 
 /*
@@ -64,26 +64,42 @@ O(mn), где m, n - длины строк.
 
 
 using System;
+using System.IO;
 
 namespace A_LevenshteinDistance
 {
     internal class Program
     {
+        private static TextReader _reader;
+        private static TextWriter _writer;
         static void Main(string[] args)
         {
-            string s1 = Console.ReadLine();
-            string s2 = Console.ReadLine();
+            InitialiseStreams();
+            string s1 = _reader.ReadLine();
+            string s2 = _reader.ReadLine();
 
             var result = GetLevenshteinDistance(s1, s2);
 
-            Console.WriteLine(result);
+            _writer.WriteLine(result);
+            CloseStreams();
         }
 
         public static int GetLevenshteinDistance(string s1, string s2)
         {
             int n = s1.Length;
             int m = s2.Length;
-            int[,] dp = new int[n + 1, m + 1];
+
+            if (n>m)
+            {
+                var temp = s1;
+                s1 = s2;
+                s2 = temp;
+                var x = n;
+                n = m;
+                m = x;
+            }
+
+            int[,] dp = new int[2, m + 1];
 
             for (int i = 0; i <= n; i++)
             {
@@ -95,21 +111,33 @@ namespace A_LevenshteinDistance
                     }
                     else if (j == 0)
                     {
-                        dp[i, j] = i;
+                        dp[i & 1, j] = i;
                     }
                     else if (i == 0)
                     {
-                        dp[i, j] = j;
+                        dp[i & 1, j] = j;
                     }
                     else
                     {
-                        int temp = Math.Min(dp[i, j - 1], dp[i - 1, j]);
-                        dp[i, j] = Math.Min(temp + 1, dp[i - 1, j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1));
+                        int temp = Math.Min(dp[i & 1, j - 1], dp[(i - 1) & 1, j]);
+                        dp[i & 1, j] = Math.Min(temp + 1, dp[(i - 1) & 1, j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1));
                     }
                 }
             }
 
-            return dp[n, m];
+            return dp[n & 1, m];
+        }
+        
+        private static void CloseStreams()
+        {
+            _reader.Close();
+            _writer.Close();
+        }
+
+        private static void InitialiseStreams()
+        {
+            _reader = new StreamReader(Console.OpenStandardInput());
+            _writer = new StreamWriter(Console.OpenStandardOutput());
         }
     }
 }
